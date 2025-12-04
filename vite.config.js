a9678bfe50ea3898a179aa0d1a00c56d61a55d03
Prasad Madushanka Dauglas Dambure Liyanage <prasad.dream13@gmail.com>
2025-12-04 21:30:23 +0100

    Added CI/CD Automation to github pages

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/AeroDeskLive/", // This is crucial for GitHub Pages
-  build: {
-    outDir: "dist", // This is the default, but good to specify
-  },
+  base: "/AeroDeskLive/",
 });
